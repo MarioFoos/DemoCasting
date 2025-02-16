df729fdad91badc47666c17526265f1c1f96761f
Mario Foos <mariofoos@gmail.com>
2025-02-16 03:19:51 -0300

    - Comentarios + Clean

--- src/DemoCasting.cpp
+++ src/DemoCasting.cpp
@@ -1,9 +1,6 @@
 //============================================================================
-// Name        : DemoCasting.cpp
-// Author      : Mario Foos
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Autor: Diardon
+// Blog : https://diardon.wordpress.com/
 //============================================================================
 
 #include <iostream>
